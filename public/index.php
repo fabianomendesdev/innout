8b93c1b77f3a63bbf3aa397202fdb8d630731e31
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-07-22 01:55:04 -0300

    updates

--- public/index.php
+++ public/index.php
@@ -3,5 +3,9 @@ require_once(dirname(__FILE__, 2). '/src/config/config.php');
 require_once(dirname(__FILE__, 2). '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
+// echo $user->getSelect('id,name');
 
-print_r($user);
+echo User::getSelect(['id' => 1], 'name, email');
+echo '<br>';
+echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+
--- src/models/Model.php
+++ src/models/Model.php
@@ -12,16 +12,46 @@ class Model {
     public function loadFromArray($arr){
         if(isset($arr)){
             foreach($arr as $key => $value){
-                $this->set($key,$value);
+                $this->$key = $value;
             }
         }
     }
 
-    public function get($key){
+    public function __get($key){
         return $this->values[$key];
     }
 
-    public function set($key, $value){
+    public function __set($key, $value){
         $this->values[$key] = $value;
+    }
+
+    public static function getSelect($filters = [],$columns = '*') {
+        $sql = "SELECT $columns FROM ". static::$tableName. static::getFilters($filters);
+        return $sql;
+    }
+
+    private static function getFilters($filters) {
+        $sql = '';  
+        if(count($filters) > 0){
+            $sql .= " WHERE";
+            foreach($filters as $key => $value){
+                $value = static::getFormatedValue($value);
+                $sql .= " $key = $value";
+                if($key !== array_key_last($filters)){
+                    $sql .= " and";
+                }
+            }
+        }
+        return $sql;
+    }
+
+    private static function getFormatedValue($value){
+        if(is_null($value)){
+            return "null";
+        }else if (is_string($value)){
+            return "'$value'";
+        }else {
+            return $value;
+        }
     }
 }
