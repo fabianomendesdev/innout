9782ae4297efae468fbc54afb0f238c02f574d58
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-08-01 13:11:01 -0300

    time update on page

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -7,9 +7,6 @@ loadModel('WorkingHours');
 $date = (new DateTime())->getTimestamp();
 $today = strftime('%d de %B de %Y', $date);
 
-$user = $_SESSION['user'];
-$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
-
 loadTemplateView('day_records', [
     'today' => $today,
     'records' => $records
--- src/views/template/left.php
+++ src/views/template/left.php
@@ -31,7 +31,7 @@
         <div class="sidebar-widget">
             <i class="icon icofont-hour-glass text-primary"></i>
             <div class="info">
-                <span class="main text-primary">04:23</span>
+                <span class="main text-primary" <?= $activeClock === 'workedInterval' ? 'active-clock' : '' ?>><?= $workedInterval ?></span>
                 <span class="label text-muted">Horas Trabalhadas</span>
             </div>
         </div>
@@ -39,7 +39,7 @@
         <div class="sidebar-widget">
             <i class="icon icofont-ui-alarm text-danger"></i>
             <div class="info">
-                <span class="main text-danger">18:00</span>
+                <span class="main text-danger" <?= $activeClock === 'exitTime' ? 'active-clock' : '' ?>><?= $exitTime ?></span>
                 <span class="label text-muted">Hora de Saída</span>
             </div>
         </div>
