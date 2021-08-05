8c1d274d5fb43e869a069a458bbae034d3876081
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-08-05 19:31:28 -0300

    update in monthly report

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -3,17 +3,35 @@ session_start();
 requireValidSession();
 
 $currentDate = new DateTime();
-
 $user = $_SESSION['user'];
-$registries = WorkingHours::getMonthlyReport($user->id,$currentDate);
+$users = null;
+$selectedUserId = $user->id;
+if($user->is_admin) {
+    $users = User::get();
+    $selectedUserId = isset($_POST['user']) ? $_POST['user'] : $user->id; 
+}
+
+$selectPeriod = isset($_POST['period']) ? new DateTime($_POST['period']) : $currentDate;
+$periods = [];
+for($yearDiff = 0; $yearDiff <= 2; $yearDiff++){
+    $year = new DateTime();
+    $year->modify("-$yearDiff year");
+
+    for($month = 12; $month >= 1; $month--){
+        $date = new DateTime(strftime('%Y',$year->getTimestamp())."-$month-1");
+        $periods[$date->format('Y-m')] = strftime('%B de %Y', $date->getTimestamp());
+    }
+}
+
+$registries = WorkingHours::getMonthlyReport($selectedUserId, $selectPeriod);
 
 $report = [];
 $workday = 0;
 $sumOfWorkedTime = 0;
-$lastDay = getLastDayOfMonth($currentDate)->format('d');
+$lastDay = getLastDayOfMonth($selectPeriod)->format('d');
 
 for($day = 1; $day <= $lastDay; $day++) {
-    $date = $currentDate->format('Y-m') . '-' . sprintf('%02d', $day);
+    $date = $selectPeriod->format('Y-m') . '-' . sprintf('%02d', $day);
     $registry = $registries[$date];
 
     if(isPastWorkday($date)) $workDay++;   
@@ -37,5 +55,9 @@ $sing = ($sumOfWorkedTime >= $expectedTime) ? '+' : '-';
 loadTemplateView('monthly_report',[
     'report' => $report,
     'sumOfWorkedTime' => getTimeStringFromSeconds($sumOfWorkedTime),
-    'balance' => "{$sing}{$balance}"
+    'balance' => "{$sing}{$balance}",
+    'selectedPeriod' => $selectPeriod->format('Y-m'),
+    'periods' => $periods,
+    'selectedUserId' => $selectedUserId,
+    'users' => $users
 ]);
