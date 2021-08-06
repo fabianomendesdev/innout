6f05bed2c967b598b54b5873fb821fd62533b2a7
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-08-06 00:20:53 -0300

    Start of Manager Report front end

--- src/models/User.php
+++ src/models/User.php
@@ -4,6 +4,6 @@ class User extends Model {
     protected static $columns = ['id','name','password','email','start_data','end_date','is_admin'];    
 
     public static function getActiveUsersCount(){
-        return static::getCount(['raw' => 'and_date IS NULL']);
+        return static::getCount(['raw' => 'end_date IS NULL']);
     }
 }
